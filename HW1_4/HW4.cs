54a7441faac84deec9c09ea8e8a17e3d8124a304
IVANNNNN <gravemail@yandex.ru>
2022-08-06 12:34:41 +0300

    Homework 4

---
+++ HW1_4/HW4.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Программа вывода четных числа");
+Console.WriteLine("Введите число ");
+string? numberString = Console.ReadLine ();
+
+int number = int.Parse(numberString!);
+
+int i = 0;
+
+if (number < 0) 
+{
+  int number1 = number * -1;
+  while (i <= number1) 
+
+  {
+    int x = i%2;
+
+    if (x == 1) 
+    { i++; }
+
+    else 
+    if ( i == 0 ) {Console.WriteLine (i++);}
+    else { Console.WriteLine ( "-" + i++); };
+
+  }
+
+}
+else 
+{
+
+ while (i <= number) 
+
+ {
+   int x = i%2;
+
+    if (x == 1) 
+     { i++; }
+
+    else 
+      { Console.WriteLine (i++); }
+  
+  }
+}

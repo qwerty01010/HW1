884b8cf9f343a637f6c901ebc8e5a182bac4cff6
IVANNNNN <gravemail@yandex.ru>
2022-08-06 12:03:37 +0300

    Homework 3

---
+++ HW1_3/HW3.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Программа проверки четности числа");
+Console.WriteLine("Введите целое число ");
+string? numberString = Console.ReadLine ();
+
+int number = int.Parse(numberString!);
+
+int x = number%2;
+
+if (x == 1)
+
+{
+    
+  Console.Write (number + " - это нечетное число");
+
+}
+else
+{
+   Console.Write (number + " - это четное число");
+}
